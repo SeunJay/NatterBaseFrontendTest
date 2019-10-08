0fd23d67364a819d91bd6e848db82307575381ff
seun jay <seunjay92@gmail.com>
2019-10-08 11:34:01 +0100

    create Dashboard component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import NavBar from "../navbar/NavBar";
+import Body from "../body/Body"
+
+const Dashboard = () => {
+  return (
+    <Fragment>
+      <NavBar />
+      <Body name="Lois"/>
+    </Fragment>
+  )
+}
+
+export default Dashboard

6e75cb05c95f31f60cbd93b8cc56a805218bf8af
seun jay <seunjay92@gmail.com>
2019-10-08 11:33:26 +0100

    import Body component

---
+++ src/components/body/Body.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+const Body = ({name}) => {
+  return (
+    <Fragment>
+      <div className="container">
+        
+      </div>
+    </Fragment>
+  )
+}
+
+export default Body

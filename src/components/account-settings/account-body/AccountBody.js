354fe83d6bb22dcf480cdc2ac862447df46c1c3c
seun jay <seunjay92@gmail.com>
2019-10-09 12:00:40 +0100

    create AccountBody component

---
+++ src/components/account-settings/account-body/AccountBody.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AccountCaption from "../accountCaption/AccountCaption";
+import "./AccounBody.css"
+
+const AccountBody = () => {
+  return (
+    <>
+      <div className="account-body">
+        <AccountCaption />
+      </div>
+    </>
+  );
+};
+
+export default AccountBody;

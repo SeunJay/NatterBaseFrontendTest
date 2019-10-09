c3a3d40d3a931a0a7ebe21edce59762e20fb1b5b
seun jay <seunjay92@gmail.com>
2019-10-09 15:09:18 +0100

    import PersonalInfo component

--- src/components/account-settings/account-body/AccountBody.js
+++ src/components/account-settings/account-body/AccountBody.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AccountCaption from "../accountCaption/AccountCaption";
-import "./AccounBody.css"
+import "./AccounBody.css";
+import PersonalInfo from "../personal-info/PersonalInfo";
 
 const AccountBody = () => {
   return (
     <>
       <div className="account-body">
         <AccountCaption />
+        <PersonalInfo />
       </div>
     </>
   );

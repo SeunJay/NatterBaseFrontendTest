ab6d75c8589e69b7156e0461dc25b85a738a3902
seun jay <seunjay92@gmail.com>
2019-10-09 12:01:29 +0100

    create AccountCaption

---
+++ src/components/account-settings/accountCaption/AccountCaption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./accountCaption.css"
+
+const AccountCaption = () => {
+  return (
+    <>
+     <h3 className="account-caption">ACCOUNT SETTINGS</h3> 
+    </>
+  )
+}
+
+export default AccountCaption

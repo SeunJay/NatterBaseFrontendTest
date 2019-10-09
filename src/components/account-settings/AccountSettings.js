2ab02aa5199b8aca013629a248518282886a59a9
seun jay <seunjay92@gmail.com>
2019-10-09 11:59:46 +0100

    import NavBar and AccountBody component

--- src/components/account-settings/AccountSettings.js
+++ src/components/account-settings/AccountSettings.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../navbar/NavBar";
+import AccountBody from "./account-body/AccountBody";
 
 const AccountSettings = () => {
   return (
     <div>
-      hshshshsh
+      <NavBar />
+      <AccountBody />
     </div>
-  )
-}
+  );
+};
 
-export default AccountSettings
+export default AccountSettings;

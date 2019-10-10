f99e351d527fa2afa07fb35c8dba11a8a3d2a90a
seun jay <seunjay92@gmail.com>
2019-10-10 11:02:11 +0100

    refactor actions to fetch applications data

--- src/actions/applicaation.js
+++ src/actions/applicaation.js
@@ -1,14 +1,4 @@
-import { GET_APPLICATIONS, APPLICATIONS_ERROR, SET_TOKEN } from "./types";
-
-const setToken = payload => ({
-  type: SET_TOKEN,
-  payload
-});
-
-const setError = payload => ({
-  type: APPLICATIONS_ERROR,
-  payload
-});
+import { GET_APPLICATIONS } from "./types";
 
 const fetchApplications = payload => ({
   type: GET_APPLICATIONS,
@@ -16,18 +6,7 @@ const fetchApplications = payload => ({
 });
 
 export const getApplications = request => async dispatch => {
-  try {
-    const res = await request.get("/get-applications");
-    dispatch(fetchApplications(res.data.applications));
-    dispatch(
-      setToken(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYjg2NWZmMzFhZWRkMzQxNDU0OTQ2YiIsImlhdCI6MTU2NTk0Nzg1NCwiZXhwIjoxNTY1OTU1MDU0fQ.5ujc_dMloqYdyoQN556PhGwIqa1OfGwt06vD5iKu-lg"
-      )
-    );
-    return res.data.applications;
-  } catch (error) {
-    return dispatch(setError(error));
-  }
+  dispatch(fetchApplications(request.get("/get-applications")));
 };
 
 export default getApplications;

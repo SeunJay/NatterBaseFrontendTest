97fb44f20292097a60981c92e04beda6b187a769
seun jay <seunjay92@gmail.com>
2019-10-10 11:02:38 +0100

    refactor action to fetch stats

--- src/actions/stats.js
+++ src/actions/stats.js
@@ -1,39 +1,12 @@
-import { GET_STATS, STATS_ERROR, SET_TOKEN, GET_STATS_LOADING } from "./types";
-
-const setToken = payload => ({
-  type: SET_TOKEN,
-  payload
-});
-
-export const setLoading = payload => ({
-  type: GET_STATS_LOADING,
-  payload
-});
-
-const setError = payload => ({
-  type: STATS_ERROR,
-  payload
-});
+import { GET_STATS } from "./types";
 
 const fetchStats = payload => ({
   type: GET_STATS,
   payload
 });
 
-export const getStats = request => async dispatch => {
-  try {
-    const res = await request.get("/get-statistics");
-    dispatch(fetchStats(res.data.statistics));
-    dispatch(setLoading(false))
-    dispatch(
-      setToken(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYjg2NWZmMzFhZWRkMzQxNDU0OTQ2YiIsImlhdCI6MTU2NTk0Nzg1NCwiZXhwIjoxNTY1OTU1MDU0fQ.5ujc_dMloqYdyoQN556PhGwIqa1OfGwt06vD5iKu-lg"
-      )
-    );
-    return res.data.statistics;
-  } catch (error) {
-    return dispatch(setError(error));
-  }
+export const getStats = request => dispatch => {
+  dispatch(fetchStats(request.get("/get-statistics")));
 };
 
 export default getStats;

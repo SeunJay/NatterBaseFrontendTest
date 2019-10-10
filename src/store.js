928352d6372b505a9cd34ee9cdda60d159f39e60
seun jay <seunjay92@gmail.com>
2019-10-10 11:01:11 +0100

    import promise from redux-promise-middleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 import rootReducer from "./reducers";
 
-const middleware = applyMiddleware(thunk);
+const middleware = applyMiddleware(thunk, promise);
 const enhancer = composeWithDevTools(middleware);
 const store = createStore(rootReducer, enhancer);
 

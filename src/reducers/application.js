acfdbb9a681b01c03781fa72eb4f2f188387f49e
seun jay <seunjay92@gmail.com>
2019-10-10 11:04:00 +0100

    refactor application reducer

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,21 +1,32 @@
-import { GET_APPLICATIONS, APPLICATIONS_ERROR } from "../actions/types";
+import { GET_APPLICATIONS } from "../actions/types";
 
 export const initialState = {
   applications: [],
+  loading: false,
+  loaded: false,
   error: {}
 };
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
-    case GET_APPLICATIONS:
+    case `${GET_APPLICATIONS}_PENDING`:
       return {
         ...state,
-        applications: payload
+        loading: true
+      }
+    case `${GET_APPLICATIONS}_FULFILLED`:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        applications: payload.data.applications
       };
-    case APPLICATIONS_ERROR:
+    case `${GET_APPLICATIONS}_REJECTED`:
       return {
         ...state,
+        loading: false,
+        loaded: false,
         error: payload
       };
     default:
